3c69c2d43c1c2cf65affb1947a7b79df7d48cdb9
Vlada Zivkovic <zivkovicv107@gmail.com>
2020-07-09 20:52:28 +0200

    HTML and CSS changes

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+#mySite 
+This is my new site
+=======
+# mySite
+New site
+>>>>>>> e6b6ac3be8d9fa0c3761c587e064007dae16cbc0
---
+++ javascript/myscripts.js
@@ -0,0 +1,47 @@
+
+        // The data/time we want to countdown to
+         function main (){
+        var yourDate = document.getElementById("Click here").value;
+        yourDate = new Date(yourDate);
+        alert ("your value is : " + yourDate);
+        var button = document.getElementById("Submit");
+        var countDownDate = new Date(yourDate).getTime();
+        if  (isNaN(yourDate)) {
+            alert("Enter you date again: ");
+             return;
+         }
+        dsbld = document.getElementById("myButton").disabled = true;
+        
+        // Run myfunc every second
+        var myfunc = setInterval(function() {
+
+        var now = new Date().getTime();
+        var timeleft = countDownDate - now;
+        
+        
+
+        // Calculating the days, hours, minutes and seconds left
+        var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+
+        // Result is output to the specific element
+        document.getElementById("days").innerHTML = days + "d "
+        document.getElementById("hours").innerHTML = hours + "h "
+        document.getElementById("mins").innerHTML = minutes + "m "
+        document.getElementById("secs").innerHTML = seconds + "s "
+
+        // Display the message when countdown is over
+        if (timeleft < 0) {
+            clearInterval(myfunc);
+            document.getElementById("days").innerHTML = ""
+            document.getElementById("hours").innerHTML = ""
+            document.getElementById("mins").innerHTML = ""
+            document.getElementById("secs").innerHTML = ""
+            document.getElementById("end").innerHTML = "TIME UP!!";
+        }
+        }, 1000); }
+
+    
